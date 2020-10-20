d0d61ec174f436a2b2fd7d280a421d0f567a1563
MasatakaTsuji <mstk73@yahoo.co.jp>
2020-10-20 17:32:30 +0900

    単体テストコード作成　user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true, length: { maximum: 40 }
+  validates :lname, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :fname, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :lname_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/}
+  validates :fname_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/}
+  validates :birthday, presence: true
+
+  validates :password,length: { minimum: 6},
+            format: { with: /\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[\d])\w{6,12}\z/}
+             
+
+
+
+  #has_many :items
+  #has_many :purchases
 end
