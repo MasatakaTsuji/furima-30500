6b35ec39e78f613f6672fcca79a24b88be909ad7
MasatakaTsuji <mstk73@yahoo.co.jp>
2020-10-20 21:58:37 +0900

    コントローラーの仮置き

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,27 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+
   def index
   end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+
+  private
+
 end
