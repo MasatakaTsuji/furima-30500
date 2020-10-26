d6b597ef30c002d42e0f06fd798dd2c5cdd86a2f
MasatakaTsuji <mstk73@yahoo.co.jp>
2020-10-26 17:27:07 +0900

    Revert "Revert "商品一覧機能""
    This reverts commit 4e40a472fba2660bc2d273545fc4bc2095054dad.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,9 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
-  #def index
-    #@item = Item.all
-    #@item = Item.order('created_at DESC')
-  #end
+  def index
+    @items = Item.order('created_at DESC')
+  end
 
   def new
     @item = Item.new
