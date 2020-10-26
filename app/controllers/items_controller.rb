c2b65f9844c059f4dab24c9df1eb598b292ab6cd
MasatakaTsuji <mstk73@yahoo.co.jp>
2020-10-26 12:41:30 +0900

    訂正1
    アクティブハッシュの1以外投稿でいないを追加した。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,10 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
-  def index
-    @item = Item.all
-    @item = Item.order('created_at DESC')
-  end
+  #def index
+    #@item = Item.all
+    #@item = Item.order('created_at DESC')
+  #end
 
   def new
     @item = Item.new
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -14,54 +14,86 @@ RSpec.describe Item, type: :model do
 
     context '投稿がうまくいかないとき' do
       it'nameが空では投稿できない'do
-       @item.name = nil
+       @item.name = ""
        @item.valid?
        expect(@item.errors.full_messages).to include("Name can't be blank")
       end
       it'descriptionが空では投稿できない'do
-      @item.description = nil
+      @item.description = ""
       @item.valid?
       expect(@item.errors.full_messages).to include("Description can't be blank")
       end
+
       it'category_idが空では投稿できない'do
-      @item.category_id = nil
+      @item.category_id = ""
       @item.valid?
-      expect(@item.errors.full_messages).to include("Category_id can't be blank")
+      expect(@item.errors.full_messages).to include("Category can't be blank","Category is not a number")
       end
+
+      it'category_idが1では投稿できない'do
+      @item.category_id = "1"
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Category must be other than 1")
+      end
+
       it'status_idが空では投稿できない'do
-      @item.status_id = nil
+      @item.status_id = ""
       @item.valid?
-      expect(@item.errors.full_messages).to include("Status_id can't be blank")
+      expect(@item.errors.full_messages).to include("Status can't be blank", "Status is not a number")
       end
+      it'status_idが1では投稿できない'do
+      @item.status_id = "1"
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Status must be other than 1")
+      end
+
       it'burden_idが空では投稿できない'do
-      @item.burden_id = nil
+      @item.burden_id = ""
       @item.valid?
-      expect(@item.errors.full_messages).to include("Burden_id can't be blank")
+      expect(@item.errors.full_messages).to include("Burden can't be blank", "Burden is not a number")
       end
+      it'burden_idが1では投稿できない'do
+      @item.burden_id  = "1"
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Burden must be other than 1")
+      end
+
       it'prefecture_idが空では投稿できない'do
-      @item.prefecture_id = nil
+      @item.prefecture_id = ""
       @item.valid?
-      expect(@item.errors.full_messages).to include("Prefecture_id can't be blank")
+      expect(@item.errors.full_messages).to include("Prefecture can't be blank", "Prefecture is not a number")
       end
+      it'prefecture_idが1では投稿できない'do
+      @item.prefecture_id = "1"
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Prefecture must be other than 1")
+      end
+
       it'ship_date_idが空では投稿できない'do
-      @item.ship_date_id = nil
+      @item.ship_date_id = ""
       @item.valid?
-      expect(@item.errors.full_messages).to include("Ship_date_id can't be blank")
-      end      
+      expect(@item.errors.full_messages).to include("Ship date can't be blank", "Ship date is not a number")
+      end
+      it'ship_date_idが1では投稿できない'do
+      @item.ship_date_id = "1"
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Ship date must be other than 1")
+      end
+
       it'selling_priceが空では投稿できない'do
-      @item.selling_price = nil
+      @item.selling_price = ""
       @item.valid?
-      expect(@item.errors.full_messages).to include("Selling_price can't be blank")
+      expect(@item.errors.full_messages).to include("Selling price can't be blank", "Selling price is invalid", "Selling price is not a number")
       end
       it'selling_priceが300円以下では投稿できない'do
-      @item.selling_price = 200
+      @item.selling_price = 299
       @item.valid?
-      expect(@item.errors.full_messages).to include("Selling_price can't be blank")
+      expect(@item.errors.full_messages).to include("Selling price must be greater than or equal to 300")
       end
       it'selling_priceが9999999円以上では投稿できない'do
       @item.selling_price = 1000000000
       @item.valid?
-      expect(@item.errors.full_messages).to include("Selling_price can't be blank")
+      expect(@item.errors.full_messages).to include("Selling price must be less than or equal to 9999999")
       end
     end
   end
